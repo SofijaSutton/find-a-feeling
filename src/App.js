52f007fe2c3cd65cae6dfd8d2bd4efacc20f1f7a
Sofija Sutton <srlsutton@gmail.com>
2025-04-16 21:07:39 -0400

    add icons and boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import logo from './find-a-feeling-no-background.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="Find-a-Feeling">
+        <img src={logo}  className="App-logo" alt="find-a-feeling logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Find-a-Feeling
       </header>
     </div>
   );
